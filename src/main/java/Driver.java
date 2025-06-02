1382ecd88b9e3eba49c543878de3e467666f98dc
Mohammad Dhimas Afrizal <mdhimas25@gmail.com>
2025-06-03 00:16:23 +0700

    aw

---
+++ src/main/java/Driver.java
@@ -0,0 +1,20 @@
+import src.main.java.Counter;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		Counter counter = new Counter();
+		
+		System.out.println("Hitung:" + counter.getCount());
+		
+		counter.increment();
+		
+		System.out.println("Hitung:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Hitung:" + counter.getCount());
+		
+	}
+}
